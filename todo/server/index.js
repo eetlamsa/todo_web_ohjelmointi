d11ad709f6eef2399a0a19f97215e1769af8ec4c
Eetu Lämsä <lamsaeetu01@gmail.com>
2026-09-06 12:38:35 +0300

    vaiheet 2-5

---
+++ todo/server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import todoRouter from './routes/todoRouter.js'
+
+const port = process.env.PORT || 3001
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/tasks', todoRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.status || 500
+    if (statusCode === 500) {
+        console.error(err.message)
+    }
+    res.status(statusCode).json({
+        error: {
+            message: err.message,
+            status: statusCode
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
+
